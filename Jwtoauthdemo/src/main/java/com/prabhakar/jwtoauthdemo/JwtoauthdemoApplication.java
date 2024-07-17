5014a1218ea650bca3c42863596b354487eef35f
Prabhakar Thakur <electrospot7@gmail.com>
2024-07-17 19:05:41 +0530

    First commit

---
+++ Jwtoauthdemo/src/main/java/com/prabhakar/jwtoauthdemo/JwtoauthdemoApplication.java
@@ -0,0 +1,13 @@
+package com.prabhakar.jwtoauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtoauthdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtoauthdemoApplication.class, args);
+	}
+
+}
---
+++ Jwtoauthdemo/src/test/java/com/prabhakar/jwtoauthdemo/JwtoauthdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prabhakar.jwtoauthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtoauthdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
